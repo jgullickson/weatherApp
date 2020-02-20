8ba49a4b9372628adcc12da1993f2766dbf508d4
James Gullickson <jgullick@umn.edu>
2020-02-20 14:13:24 -0600

    basic functionality implemented

---
+++ main.js
@@ -0,0 +1,27 @@
+const KEY = '6fa2ee15e2da46c3b7c184521202002';
+// let DATA = null;
+LOCATION = '';
+
+async function getWeatherByLocation(event) {
+    console.log(event)
+    let url = `http://api.weatherapi.com/v1/current.json?key=${KEY}&q=${LOCATION}`;
+    fetch(url)
+        .then(response =>{
+            return response.json()
+        }).then(data => {
+            console.log(data)
+            document.getElementById('location-title').innerHTML = data.location.name;
+            document.getElementById('location-region').innerHTML = `${data.location.region}, `;
+            document.getElementById('location-country').innerHTML = data.location.country;
+            document.getElementById('weather-icon').src = data.current.condition.icon;
+            document.getElementById('weather-condition').innerHTML = data.current.condition.text;
+            document.getElementById('temp').innerHTML = `${data.current.temp_f} &deg; F`;
+        })
+}
+
+function setLocation(event){
+    LOCATION = event.target.value
+}
+
+// getWeatherByLocation('saint paul');
+// console.log(DATA)
